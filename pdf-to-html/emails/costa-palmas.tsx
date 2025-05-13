5a2893607bd268f40f4ca87b045a7a662b06742b
stteeel <samiLiebre@proton.me>
2025-05-13 15:18:03 -0400

    replace img

--- pdf-to-html/emails/costa-palmas.tsx
+++ pdf-to-html/emails/costa-palmas.tsx
@@ -230,9 +230,9 @@ export default function CostapalmasEmail() {
 								className="no-underline text-white text-[0.95rem] tracking-[0.08em] font-normal inline-block leading-[1.3] font-['Basis_Grotesque_Pro_Light',_Arial,_sans-serif]"
 							>
 								<Img
-									src="https://raw.githubusercontent.com/saimaneim/PDF-to-HTML/refs/heads/main/pdf-to-html/static/text-footer_1.png"
+									src="https://raw.githubusercontent.com/saimaneim/PDF-to-HTML/refs/heads/main/pdf-to-html/static/text_footer_1.png"
 									alt="Instagram"
-									className="w-full h-[16px] inline-block"
+									className="w-full h-[20px] inline-block"
 								/>
 							</Link>
 							<br />
